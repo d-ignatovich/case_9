33e578b3d91702f0b8c7e16de9bebc4832f31d76
Helenpoilova <73345948+Helenpoilova@users.noreply.github.com>
2021-05-22 20:57:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,18 @@
 Принимая ежегодные решения вы будете либо оставаться у власти, либо будете свергнуты своим народом.
 Покажите на что вы способны! Удачи!
 '''
+print("Have you ever dreamed of running a state? You have a great opportunity to show how you are. You are in power in the state ***."+"\n"+ 
+"By making annual decisions, you will either stay in power or be overthrown by your people."+"\n"+"Show what you are capable of! Good luck!"+"\n"+"You have certain parameters: land, treasury, wheat, people, troubles.")
+def main():
+    for key, value in dictionary:
+        if land[value] < 300:
+            return print("Game over. They ruled the state ***"+ year)
+        elif population[value] < 200:
+            return print("Game over. They ruled the state ***"+ year)
+        elif the_level_of_turmoil[value] > 50:
+            return print("Game over. They ruled the state ***"+ year)
+        elif treasury[value] =="bankrupt":
+            return print("Game over. They ruled the state ***"+ year)
+        else:
+            continue
+main()
