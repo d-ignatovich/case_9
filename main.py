394897401aa6b700fca263d2971f9d55f97bd0ab
d-ignatovich <73305627+d-ignatovich@users.noreply.github.com>
2021-05-23 21:26:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,46 @@ def main():
         else:
             continue
 main()
+
+
+
+
+
+
+
+import random
+
+print("Have you ever dreamed of running a state? You have a great opportunity to show how you are. You are in power "
+      "in the state N. By making annual decisions, you will either stay in power or be overthrown by your people. "
+      "Show what you are capable of! Good luck! You have certain parameters: territory, treasury, wheat, people, "
+      "troubles.")
+def main():
+    year = 1
+    dict = {'territory': 0, 'treasury': 1000, 'people': 1000, 'wheat': 5000, 'troubles': 0}
+    for k, v in dict.items():
+        print(k, ':', v, sep ='', end='  ')
+
+    end_ter = random.randint(0, 300)
+    end_people = random.randint(0, 200)
+    end_troub = random.randint(50, 100)
+    end_wheat = random.randint(1, 3)
+    check_game(end_ter, end_people, end_troub, end_wheat, year)
+
+
+def check_game(end_ter, end_people, end_troub, end_wheat, year):
+    if dict['territory'] < end_ter:
+        return print("Game over. You've lost territory. You ruled the state N"+ year)
+    elif dict['treasury'] < 0:
+        return print("Game over. The treasury is empty. You ruled the state N"+ year)
+    elif dict['people'] < end_people:
+        return print("Game over. Not enough people. You ruled the state N"+ year)
+    elif dict['troubles'] > end_troub:
+        return print("Game over. You were deposed. You ruled the state N"+ year)
+    elif dict['wheat'] // dict['people'] < end_wheat:
+        a = random.randint(0, 10)
+        dict['troubles'] += a
+        return print('Hunger. The level of troubles increased by', a)
+    return year+1
+
+
+main()
